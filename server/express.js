d7300edf91c250d85492938f3a2dc10c01e6f024
chsiao <chsiao3@my.centennialcollege.ca>
2025-07-20 00:09:44 -0400

    fix express.js

--- server/express.js
+++ server/express.js
@@ -4,29 +4,43 @@ import cookieParser from "cookie-parser";
 import compress from "compression";
 import cors from "cors";
 import helmet from "helmet";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import userRoutes from "./routes/user.routes.js";
 import newsRoutes from "./routes/news.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 
-
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use("/", userRoutes);
-app.use("/", newsRoutes);
-app.use("/", authRoutes);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compress());
 app.use(helmet());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
+
+app.use("/api", userRoutes);
+app.use("/api", newsRoutes);
+app.use("/auth", authRoutes);
+
 app.use((err, req, res, next) => {
   if (err.name === "UnauthorizedError") {
     res.status(401).json({ error: err.name + ": " + err.message });
   } else if (err) {
     res.status(400).json({ error: err.name + ": " + err.message });
-    console.log(err);
+    console.error(err);
   }
 });
 
